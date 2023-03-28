df09c8c22ba0b57e6d84fcdfb0c4316f6190d70b
MURODJON <joraev_m98@mail.ru>
2023-03-28 13:49:10 +0500

    MiCro

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+import { Product } from './product/entities/product.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'product',
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductModule
+  ],
+  controllers: [],
+  providers: []
+  
+})
+export class AppModule {}
